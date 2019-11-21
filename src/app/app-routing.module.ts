9e78f32718d34c8876f3dc3cef89cadd7894c0c4
gutolpn <gutolpn@gmail.com>
2019-11-21 11:34:16 -0300

    add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {LancamentosComponent} from './lancamentos/lancamentos.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'lancamentos',
     component: LancamentosComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 
