eca2c23eab27b8e958063bcbd2a5f81749c06d05
gutolpn <gutolpn@gmail.com>
2019-11-28 11:28:06 -0300

    versão final

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,10 +12,10 @@ export class HomeComponent implements OnInit {
 
   constructor() {
     this.produtos = [
-      new Produto('Caneta', 10.00),
-      new Produto('Caderno', 15.00),
-      new Produto('Caneca', 5.00),
-      new Produto('Carteira', 25.00)
+      new Produto('Caneta', 10.00, 'http://encurtador.com.br/AG136'),
+      new Produto('Caderno', 15.00, 'http://encurtador.com.br/zBEMQ'),
+      new Produto('Caneca', 5.00, 'http://encurtador.com.br/ipIU1'),
+      new Produto('Carteira', 25.00, 'http://encurtador.com.br/ruEQ3')
     ];
   }
 
